37eb9a150803af8a5734ddbb3aefcb863548679e
Nor sairi bin ahmad shahabudin <maxsim86@gmail.com>
2024-02-16 11:57:57 +0800

    add deleView func

--- booking_app/views.py
+++ booking_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room, Reservation
 from datetime import datetime
+from django.views import View
 
 
 # Create your views here.
@@ -40,4 +41,68 @@ def room(request, id):
     }
     
     return render(request, 'booking/room.html', ctx)
+
+
+class NewRoomView(View):
+    def get(self, request):
+        
+        try:
+            name = request.POST.get("name")
+            capacity = request.POST.get("capacity")
+            projector = request.POST.get("projector")
+            proj = True if projector == 'True' else False
+            
+            Room.objects.create(name=name, capacity=capacity, projector=proj)
+
+            return redirect("/bookconfroom/")
+        except Exception as e:
+            message = "Data tidak betul: {}".format(e)
+            ctx = {
+                "message":message,
+            }
+            return render(request, "booking/new_room.html", ctx)
+
+class ModifyView(View):
+    def get(self, request, id):
+        room = Room.objects.get(pk=id)
+        ctx = {
+            "room": room,
+        }
+        return render(request, 'booking/modify.html', ctx)
+    
+    def post(self, request, id):
+        name = request.POST.get("name")
+        capacity = request.POST.get("capacity")
+        projector = True if request.POST.get('projector') else False
+        room = Room.objects.get(pk=id)
+        try:
+            room.name =name
+            room.capacity = capacity
+            room.projector = projector
+            room.save()
+            return redirect('/bookconfroom')
+        except Exception as e:
+            message = "Data tidak betul: {}".format(e)
+            ctx = {
+                "message": message,
+                "room": room,
+            }
+            return render(request, 'booking/modify.html', ctx)
+        
+class DeleteView(View):
+    def get(self, request, id):
+        room = Room.objects.get(pk=id)
+        ctx = {
+            "room":room,
+        }
+        return render(request, 'booking/delete.html', ctx)
+
+    def post(self, request, id):
+        action = request.POST.get('submit')
+
+        if action == "YA":
+            room = Room.objects.get(pk=id)
+            room.delete()
+
+        return redirect("/bookconfroom")
 
